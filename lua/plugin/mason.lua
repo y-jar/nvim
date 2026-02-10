9394ae90b7b7a724d1d5dae4555008451529b63c
y-jar <park.7qs@gmail.com>
2026-02-10 05:12:45 -0500

    fixed file arg issue

--- lua/plugin/mason.lua
+++ lua/plugin/mason.lua
@@ -4,54 +4,55 @@
 -- ========================================================================== --
 
 return {
-  {
-    "williamboman/mason.nvim",
-    dependencies = {
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
-    },
-    config = function()
-      -- Initialize Mason
-      require("mason").setup({
-        -- NEEDED FOR C# (roslyn)
-        registries = {
-          "github:mason-org/mason-registry",
-          "github:Crashdummyy/mason-registry", -- This one contains Roslyn
-        },
-        ui = {
-          border = "rounded",
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
+	{
+		"williamboman/mason.nvim",
+		dependencies = {
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+		},
+		config = function()
+			-- Initialize Mason
+			require("mason").setup({
+				-- NEEDED FOR C# (roslyn)
+				registries = {
+					"github:mason-org/mason-registry",
+					"github:Crashdummyy/mason-registry", -- This one contains Roslyn
+				},
+				ui = {
+					border = "rounded",
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
 
-      -- List of tools for Mason to install automatically
-      -- This ensures your environment is consistent across different machines.
-      local ensure_installed = {
-        -- LSPs (Language Servers)
-        "lua-language-server", -- Lua
-        "pyright",             -- Python
-        "clangd",              -- C / C++ / C+
-        "rust-analyzer",       -- Rust
-        "typescript-language-server", -- JS/TS
-        "html-lsp",            -- HTML
-        "phpactor",            -- PHP
-        "bash-language-server",-- Bash/Zsh
-        "jdtls",               -- Java
-        "roslyn-lsp",              -- C#
-        
-        -- Formatters & Linters
-        "stylua",              -- Lua formatting
-        "shfmt",               -- Shell formatting
-      }
+			-- List of tools for Mason to install automatically
+			-- This ensures your environment is consistent across different machines.
+			local ensure_installed = {
+				-- LSPs (Language Servers)
+				"lua-language-server", -- Lua
+				"pyright", -- Python
+				"clangd", -- C / C++ / C+
+				"rust-analyzer", -- Rust
+				"typescript-language-server", -- JS/TS
+				"html-lsp", -- HTML
+				"phpactor", -- PHP
+				"bash-language-server", -- Bash/Zsh
+				"jdtls", -- Java
+				"roslyn", -- C#
 
-      require("mason-tool-installer").setup({
-        ensure_installed = ensure_installed,
-        auto_update = true,
-        run_on_start = true,
-      })
-    end,
-  },
+				-- Formatters & Linters
+				"stylua", -- Lua formatting
+				"shfmt", -- Shell formatting
+			}
+
+			require("mason-tool-installer").setup({
+				ensure_installed = ensure_installed,
+				auto_update = true,
+				run_on_start = true,
+			})
+		end,
+	},
 }
+
